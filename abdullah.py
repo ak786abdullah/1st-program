718f90260dd47c25486845331a1ea0314fb103f4
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2025-04-23 22:12:22 +0500

    Add files via upload

---
+++ abdullah.py
@@ -0,0 +1,24 @@
+#float
+#dacimle number 
+weight =50.5 #in kg 
+height= 5.11 #in feet  
+print(weight,height)
+print(weight)
+print(height)
+print(weight+height)
+#string
+#sequence of text ,charactors 
+#value of variable ( string value) always in ""
+firstname ="abdullah"
+secondname="khan"
+print(firstname,secondname)
+#out put will be at one line each value separated by space 
+print(firstname)
+print(secondname)
+#every output at new lane 
+print(firstname+secondname)
+#output will be horizantal with out space 
+#is it possible we print all data type togather?
+print("10kg weight 5.11ft height ")
+#we can print all data type togather when we write value  in "" (string form ) 
+#integer =10 float=5.11 string=weight,height  
